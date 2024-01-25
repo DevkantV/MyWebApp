e81cc33d027fe4e7af2297dd4993cfc6e341bcd7
DevkantV <154890065+DevkantV@users.noreply.github.com>
2024-01-25 19:28:47 +0530

    Update Program.cs with tags and release notes

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //My first changes
 //feature/myfeature-1 branch changes.
-//Tags and release notes creation
+//Tags and release notes creation try2
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
